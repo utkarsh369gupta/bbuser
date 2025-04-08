e605769e0b577b0ded92c56b6713372de7f9615f
Utkarsh369Gupta <utk369gupta@gmail.com>
2025-04-08 16:22:38 +0530

    remove button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,9 +8,9 @@ const Navbar = () => {
                     <img className="h-auto max-w-20 transition-all duration-300 rounded-lg  filter inline hover:max-w-24" src="images/budgetbuddies.png" alt="image description" />
                     <p className="text-3xl font-bold text-white">Budget Buddy</p>
                 </div>
-                <div className='flex gap-4 text-2xl text-white cursor-pointer'>
+                {/* <div className='flex gap-4 text-2xl text-white cursor-pointer'>
                     <a href="http://budget-buddy-s3.s3-website.ap-south-1.amazonaws.com" className='text-orange-200 hover:text-orange-400'>Log Out</a>
-                </div>
+                </div> */}
             </div>
         </div>
     )
